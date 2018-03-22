259a9c22700a6502779e504f84c74e8fdab27def
Valentino Christian <val@Valentinos-MacBook-Pro.local>
2018-03-21 21:58:23 -0400

    rough draft of altering sign-in count admin privilege

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -15,6 +15,7 @@ export class AdminComponent implements OnInit {
   private users;
   public orderForm: FormGroup;
   private cands = ['President', 'VP', 'Secretary', 'Treasurer'];
+  private nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
   notifications = {
     emptyField: false,
@@ -120,6 +121,10 @@ export class AdminComponent implements OnInit {
   //GOOD JOB YO!
   public getCandidates() {
     return this.cands;
+  }
+
+  public getNums() {
+    return this.nums;
   }
 
 }
