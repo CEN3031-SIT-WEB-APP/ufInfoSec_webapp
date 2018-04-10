74de29a6fe6e861a6f3557b28dfc80e1b868b85f
Valentino Christian <val@Valentinos-MacBook-Pro.local>
2018-04-10 15:55:07 -0400

    trying to get tests to work

--- test/anonymousTest.js
+++ test/anonymousTest.js
@@ -1,10 +1,13 @@
 'use strict';
 var expect = require('chai').expect;
+var bodyParser = require('body-parser')
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var app = require('../app.js'); //this could also be exporting the wrong server, since you forced a listen at the end but there could be one earlier on
 const anonymous = require('../api/anonymous.js') //you really want to be using this since this exports routes, which is used to make the post request
 
+anonymous.use(bodyParser.json())
+anonymous.use(bodyParser.urlencoded({ extended: false }))
 chai.use(chaiHttp);
 
 const userCredentials = {
@@ -19,10 +22,10 @@ describe('anonymous.js', function() {
     it('should send parameters to : /path POST', function(done) {
         chai
             .request(anonymous)
-            .post(path)
-            .type('')
-            .send(userCredentials)
-            .end(function(error, response, body) {
+            .post(path, function(userCredentials, res, next) {
+                console.log("HERE");
+            })
+            .end(function(error, response) {
                 console.log(response);
                 expect(response).to.have.status(200);
                 if (error) {
